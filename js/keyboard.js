2858a58c47defbf5a8616a38fbf2546941a1c31a
mks <katafoxi@gmail.com>
2022-06-12 21:45:52 +0300

    перед изменением

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,13 +1,14 @@
 let ArrowRowSymbols = {
-    rowF1: ['Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', '', 'PrtSc', 'ScrLk', 'Pause'],
-    row12: ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace', 'Ins', 'home', 'PageUp'],
-    rowQW: ['', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '/', 'Del', 'End', 'PageD'],
-    rowAS: ['', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '"', '', 'Enter', '', 'Up', ''],
-    rowZX: ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '', '', '', 'Left', 'Down', 'Right']
+    rowF1: ['mod', 'Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', '', 'PSc', 'SLk', 'Pse'],
+    row12: ['d', '', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Bck', 'Ins', 'home', 'PUp'],
+    rowQW: ['md', '', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '/', 'Del', 'End', 'PD'],
+    rowAS: ['md', '', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '"', '', 'Ent', '', 'Up', ''],
+    rowZX: ['md', '', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '', '', '', 'Lft', 'Dwn', 'Rght']
 }
 
-drawButtonTable = function() {
-    //create row 1
+let modificators = ['Alt', 'Ctrl', 'Shift', 'CtrlAlt', 'CtrlShift', 'AltShift', 'CtrlAltShift'];
+
+generateKeyboardTable = function() {
     var tableHtml = '';
     var charsHtml = '<tr>';
 
@@ -20,6 +21,48 @@ drawButtonTable = function() {
 
     tableHtml += charsHtml;
     document.getElementById('keybordTable').innerHTML = tableHtml;
+}();
+
+generateModificatorsSubTable = function(flag = false) {
+    let modificatorName = '';
+
+
+    var ModificatorCellHTML = '';
+    for (modif of modificators) {
+        if (flag) modificatorName = modif;
+        ModificatorCellHTML += '<tr><td class="' + modif + 'Color + Color" >' + '</td>';
+        ModificatorCellHTML += '<td class= "' + modif + 'Comand + Comand">' + modificatorName + '</td></tr>';
+    }
+    modificatorName = '';
+    return ModificatorCellHTML;
+};
+
+addCharCellInModificatorSubTable = function(charButton) {
+    let CharClass = charButton.textContent
+
+    let addCharCellsHTML = '<tr><td class="' + CharClass + '+ Color">' + CharClass + '</td> <td class= "Comand"></td></tr>';
+    return addCharCellsHTML;
 }
 
-drawButtonTable();
+getFinalSubTable = function() {
+
+    let charArray = document.getElementsByClassName('char');
+
+    for (charButton of charArray) {
+
+        let subTableHtml = '<table id="modificatorForChar"><tbody>';
+        subTableHtml += addCharCellInModificatorSubTable(charButton);
+        if (charButton.textContent == 'mod') {
+            let flag = true;
+            subTableHtml += generateModificatorsSubTable(flag);
+        } else {
+            flag = false;
+            subTableHtml += generateModificatorsSubTable(flag);
+        }
+
+
+        subTableHtml += '</tbody></table>';
+        charButton.innerHTML = subTableHtml;
+        subTableHtml = '';
+    }
+}()
