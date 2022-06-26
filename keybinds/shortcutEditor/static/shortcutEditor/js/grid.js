b459b44a3aa28bd60c1389a324169a55af49b091
mks <katafoxi@gmail.com>
2022-06-26 23:38:00 +0300

    рефактор

--- keybinds/shortcutEditor/static/shortcutEditor/js/grid.js
+++ keybinds/shortcutEditor/static/shortcutEditor/js/grid.js
@@ -1,4 +1,4 @@
-let arrayKeyboardKeyValues = {
+const arrayKeyboardKeyValues = {
     rowF1: ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Esc', 'PSc', 'SLk', 'Pse'],
     row12: ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Bck', 'Ins', 'home', 'PUp'],
     rowQW: ['', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '/', 'Del', 'End', 'PD'],
@@ -6,10 +6,14 @@ let arrayKeyboardKeyValues = {
     rowZX: ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '', '', '', 'Lft', 'Dwn', 'Rght']
 }
 
-let modifiers = ['Alt', 'Ctrl', 'Shift', 'CtrlAlt', 'CtrlShift', 'AltShift', 'CtrlAltShift'];
+const modifiers = ['Alt', 'Ctrl', 'Shift', 'CtrlAlt', 'CtrlShift', 'AltShift', 'CtrlAltShift'];
+
+generateKeyboardGrid();
+showFlipswiths();
+showModifiersList();
 
 
-showFlipswiths = function() {
+function showFlipswiths() {
     let collectionShemeSwitchCells = document.getElementsByClassName('schemeSwitchCell');
     for (let i = 0; i < collectionShemeSwitchCells.length; i++) {
         let elem = collectionShemeSwitchCells[i];
@@ -23,12 +27,10 @@ showFlipswiths = function() {
                             </div>'
         elem.innerHTML = flipswitchKey;
     }
-}();
+};
 
 
-
-
-getModifiersList = function() {
+function showModifiersList() {
     let collectionFlipswitchKeys = document.getElementsByClassName('flipswitch-cb');
     for (let i = 0; i < collectionFlipswitchKeys.length; i++) {
         let elem = collectionFlipswitchKeys[i];
@@ -37,18 +39,18 @@ getModifiersList = function() {
             let modifierFromClassCell = elem.closest('.Cell').getAttribute('class').split(' ')[1];
             if (elem.checked) {
                 showModifiersRows(modifierFromClassCell);
-                modifierFromClassCell += '-title';
+                modifierFromClassCell = modifierFromClassCell.split('-')[0]
                 showModifiersRows(modifierFromClassCell);
             } else {
                 hideModifiersRows(modifierFromClassCell);
-                modifierFromClassCell += '-title';
+                modifierFromClassCell = modifierFromClassCell.split('-')[0]
                 hideModifiersRows(modifierFromClassCell)
             }
         });
     }
-}();
+}
 
-hideModifiersRows = function(modifier) {
+function hideModifiersRows(modifier) {
     let collectionModRow = document.getElementsByClassName(modifier);
     for (let i = 1; i < collectionModRow.length; i++) {
         let elem = collectionModRow[i];
@@ -56,7 +58,7 @@ hideModifiersRows = function(modifier) {
     }
 }
 
-showModifiersRows = function(modifier) {
+function showModifiersRows(modifier) {
     let collectionModRow = document.getElementsByClassName(modifier);
     for (let i = 1; i < collectionModRow.length; i++) {
         let elem = collectionModRow[i];
@@ -64,35 +66,35 @@ showModifiersRows = function(modifier) {
     }
 }
 
-getSubgridWithModifiers = function(keyboardKey) {
+function getSubgridWithModifiers(keyboardKey) {
     let subtableHTML = '';
     if (keyboardKey == '') {
         subtableHTML = '';
     } else {
         subtableHTML = '<div  class="subtable">\
-                            <div class="Symbol">' + keyboardKey + '</div>\
-                            <div class="Alt droppable  abbr" title = "alt">a</div>\
-                            <div class="Ctrl droppable abbr">c</div>\
-                            <div class="Shift droppable abbr">s</div>\
-                            <div class="CtrlAlt droppable abbr">ca</div>\
-                            <div class="CtrlShift droppable abbr">cs</div>\
-                            <div class="AltShift droppable abbr">as</div>\
-                            <div class="CtrlAltShift droppable abbr">cas</div>\
-                            <div class="Simple droppable"></div>\
-                            <div class="Alt-title"></div>\
-                            <div class="Ctrl-title"></div>\
-                            <div class="Shift-title"></div>\
-                            <div class="CtrlAlt-title"></div>\
-                            <div class="CtrlShift-title"></div>\
-                            <div class="AltShift-title"></div>\
-                            <div class="CtrlAltShift-title"></div>\
-                            <div class="Simple-title"></div>\
+                            <div class="brdr Symbol">' + keyboardKey + '</div>\
+                            <div class="brdr Alt abbr" title = "alt">a</div>\
+                            <div class="brdr Ctrl abbr">c</div>\
+                            <div class="brdr Shift abbr">s</div>\
+                            <div class="brdr CtrlAlt abbr">ca</div>\
+                            <div class="brdr AltShift abbr">as</div>\
+                            <div class="brdr CtrlShift abbr">cs</div>\
+                            <div class="brdr CtrlAltShift abbr">cas</div>\
+                            <div class="brdr Simple "></div>\
+                            <div class="brdr Alt-title droppable"></div>\
+                            <div class="brdr Ctrl-title droppable"></div>\
+                            <div class="brdr Shift-title droppable"></div>\
+                            <div class="brdr CtrlAlt-title droppable"></div>\
+                            <div class="brdr AltShift-title droppable"></div>\
+                            <div class="brdr CtrlShift-title droppable"></div>\
+                            <div class="brdr CtrlAltShift-title droppable"></div>\
+                            <div class="brdr Simple-title droppable"></div>\
                         </div>';
     }
     return subtableHTML;
 }
 
-generateKeyboardGrid = function() {
+function generateKeyboardGrid() {
     var KeyboardGridHTML = '';
 
     for (rowButtons in arrayKeyboardKeyValues) {
@@ -102,103 +104,48 @@ generateKeyboardGrid = function() {
     };
     // console.log(KeyboardGridHTML);
     document.getElementById('keybordGrid').innerHTML = KeyboardGridHTML;
-}();
-
-let currentDroppable = null;
-let lastCurentDroppable = null;
-let saveTextContent = '';
-
-getHandlerIcon = function(){
-    let collectionIcon = document.getElementsByClassName('icons');
-        for (let i = 1; i < collectionIcon.length; i++) {
-            let elem = collectionIcon[i];
-            elem.style.display = '';
-    }
-
 }
 
-ball.onmousedown = function(event) {
-    ball.style.cursor= 'grabbing'
+
+// dragula([document.querySelector('#left'), document.querySelector('#right')], {});
+
+const boxNodes = document.querySelectorAll('.droppable'); // returns NodeList
+
+const draggableBoxes = Array.prototype.slice.call(boxNodes);
 
 
-    let shiftX = event.clientX - ball.getBoundingClientRect().left;
-    let shiftY = event.clientY - ball.getBoundingClientRect().top;
+dragula(draggableBoxes, )
+    .on('drop', (el, targret) => {
+        showHide();
 
-    ball.style.position = 'absolute';
-    ball.style.zIndex = 1000;
-    document.body.append(ball);
+    })
 
-    moveAt(event.pageX, event.pageY);
+// function showHideDescriptionCommand(el, targret) {
+//     // alert(el.childNodes[1].offsetWidth)
+//     if (+targret.offsetWidth < 35) {
+//         // alert(el.nodeName)
+//         el.childNodes[3].style.display = 'none';
+//     } else {
+//         el.childNodes[3].style.display = '';
+//     }
+// }
 
-    function moveAt(pageX, pageY) {
-        ball.style.left = pageX - shiftX + 'px';
-        ball.style.top = pageY - shiftY + 'px';
-    }
+function showHide() {
+    let collectionCommandDescr = document.getElementsByClassName('command_description');
+    // console.log(collectionCommandDescr)
 
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
+    for (let i = 1; i < collectionCommandDescr.length; i++) {
+        let elem = collectionCommandDescr[i];
 
-        ball.hidden = true;
-        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-        ball.hidden = false;
+        let elemWidth = elem.childNodes[1].offsetWidth + elem.childNodes[3].offsetWidth;
+        // alert(elem.parentNode.offsetWidth)
 
-        if (!elemBelow) return;
-
-        let droppableBelow = elemBelow.closest('.droppable');
-        if (currentDroppable != droppableBelow) {
-
-            if (currentDroppable) { // null если мы были не над droppable до этого события
-                // (например, над пустым пространством)
-
-                leaveDroppable(currentDroppable);
-            }
-            currentDroppable = droppableBelow;
-            if (currentDroppable) { // null если мы не над droppable сейчас, во время этого события
-                // (например, только что покинули droppable)
-                enterDroppable(currentDroppable);
-            }
-        }
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-
-    ball.onmouseup = function() {
-
-        document.removeEventListener('mousemove', onMouseMove);
-        ball.onmouseup = null;
-        ball.style.cursor= 'grab'
-        if (currentDroppable) {
-
-            if (lastCurentDroppable) {
-                if (lastCurentDroppable != currentDroppable) {
-                    lastCurentDroppable.textContent = saveTextContent;
-                }
-            }
-
-            saveTextContent = currentDroppable.textContent;
-            lastCurentDroppable = currentDroppable;
-            currentDroppable.textContent = '';
-            ball.style.position = 'static';
-            currentDroppable.appendChild(ball);
-            currentDroppable.style.background = '';
-
-            ball.style.border = '';
+        if (+elem.parentNode.offsetWidth < 50) {
+            elem.childNodes[3].style.display = 'none';
         } else {
-            lastCurentDroppable.textContent = saveTextContent;
-            ball.style.border = '2px solid rgb(255, 0, 0)';
+            elem.childNodes[3].style.display = '';
         }
-    };
-};
-
-function enterDroppable(elem) {
-
-    elem.style.background = 'pink';
+    }
 }
 
-function leaveDroppable(elem) {
-    elem.style.background = '';
-}
-
-ball.ondragstart = function() {
-    return false;
-};
+window.onresize = showHide;
