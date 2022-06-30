ae7ce580716ee392307dac9bf7ee1fbe3739600f
mks <katafoxi@gmail.com>
2022-06-30 21:32:21 +0300

    рефактор + добавлены кнопки мыши

--- keybinds/shortcutEditor/static/shortcutEditor/js/grid.js
+++ keybinds/shortcutEditor/static/shortcutEditor/js/grid.js
@@ -1,20 +1,96 @@
-const arrayKeyboardKeyValues = {
+const keyboardKeys = {
     rowF1: ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Esc', 'PSc', 'SLk', 'Pse'],
     row12: ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Bck', 'Ins', 'home', 'PUp'],
     rowQW: ['', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '/', 'Del', 'End', 'PD'],
     rowAS: ['', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '„ “', '', 'Enter', '', 'Up', ''],
-    rowZX: ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '', '', '', 'Lft', 'Dwn', 'Rght']
+    rowZX: ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'mouse-lft', 'mouse-mid', 'mouse-rght', 'Lft', 'Dwn', 'Rght']
 }
 
 const modifiers = ['Alt', 'Ctrl', 'Shift', 'CtrlAlt', 'CtrlShift', 'AltShift', 'CtrlAltShift'];
 
 generateKeyboardGrid();
-showFlipswiths();
-showModifiersList();
 onDragMaster();
-window.onresize = showHide;
+window.onresize = changeCommandDescriptionWidth;
+showFlipswiths();
+showOrHideModifiersRows();
 
 
+function generateKeyboardGrid() {
+    var KeyboardGridHTML = '';
+
+    for (rowButtons in keyboardKeys) {
+        for (keyButton of keyboardKeys[rowButtons]) {
+            KeyboardGridHTML += '<div class="char' + keyButton + '">' + getSubgridWithModifiers(keyButton) + '</div>';
+        }
+    };
+    document.getElementById('keybordGrid').innerHTML = KeyboardGridHTML;
+}
+
+function getSubgridWithModifiers(keyboardKey) {
+    let subtableHTML = '';
+    if (keyboardKey == '') {
+        subtableHTML = '';
+    } else {
+        subtableHTML = '<div  class="subtable">\
+                            <div class="brdr Symbol">' + keyboardKey + '</div>\
+                            <div class="brdr Alt abbr" title = "alt">a</div>\
+                            <div class="brdr Ctrl abbr">c</div>\
+                            <div class="brdr Shift abbr">s</div>\
+                            <div class="brdr CtrlAlt abbr">ca</div>\
+                            <div class="brdr AltShift abbr">as</div>\
+                            <div class="brdr CtrlShift abbr">cs</div>\
+                            <div class="brdr CtrlAltShift abbr">cas</div>\
+                            <div class="brdr Simple "></div>\
+                            <div class="brdr Alt-title droppable"></div>\
+                            <div class="brdr Ctrl-title droppable"></div>\
+                            <div class="brdr Shift-title droppable"></div>\
+                            <div class="brdr CtrlAlt-title droppable"></div>\
+                            <div class="brdr AltShift-title droppable"></div>\
+                            <div class="brdr CtrlShift-title droppable"></div>\
+                            <div class="brdr CtrlAltShift-title droppable"></div>\
+                            <div class="brdr Simple-title ' + keyboardKey + ' droppable"></div>\
+                        </div>';
+    }
+    return subtableHTML;
+}
+
+// dragula([document.querySelector('#left'), document.querySelector('#right')], {});
+function onDragMaster() {
+    const boxNodes = document.querySelectorAll('.droppable'); // returns NodeList
+
+    const draggableBoxes = Array.prototype.slice.call(boxNodes);
+
+
+    dragula(draggableBoxes, )
+        .on('drag', (el) => {
+            el.style.width = '14px';
+        })
+        .on('drop', (el, targret) => {
+            el.style.width = (targret.offsetWidth - 5) + 'px';
+        })
+}
+
+window.onbeforeunload = function() {
+    return "Есть несохранённые изменения. Всё равно уходим?";
+};
+
+
+function changeCommandDescriptionWidth() {
+    let collectionCommandDescr = document.getElementsByClassName('command_description');
+    // console.log(collectionCommandDescr)
+
+
+    for (let i = 1; i < collectionCommandDescr.length; i++) {
+        let elem = collectionCommandDescr[i];
+
+        if (elem.parentNode.getAttribute('class').split(' ')[0] == 'brdr') {
+            elem.style.width = '14px'
+        }
+        if (elem.parentNode.getAttribute('class').split(' ')[0] == 'brdr') {
+            elem.style.width = (elem.parentNode.offsetWidth - 5) + 'px';
+        }
+    }
+}
 
 function showFlipswiths() {
     let collectionShemeSwitchCells = document.getElementsByClassName('schemeSwitchCell');
@@ -30,10 +106,10 @@ function showFlipswiths() {
                             </div>'
         elem.innerHTML = flipswitchKey;
     }
-};
+}
 
 
-function showModifiersList() {
+function showOrHideModifiersRows() {
     let collectionFlipswitchKeys = document.getElementsByClassName('flipswitch-cb');
     for (let i = 0; i < collectionFlipswitchKeys.length; i++) {
         let elem = collectionFlipswitchKeys[i];
@@ -66,90 +142,5 @@ function showModifiersRows(modifier) {
     for (let i = 1; i < collectionModRow.length; i++) {
         let elem = collectionModRow[i];
         elem.style.display = '';
-    }
-}
-
-function getSubgridWithModifiers(keyboardKey) {
-    let subtableHTML = '';
-    if (keyboardKey == '') {
-        subtableHTML = '';
-    } else {
-        subtableHTML = '<div  class="subtable">\
-                            <div class="brdr Symbol">' + keyboardKey + '</div>\
-                            <div class="brdr Alt abbr" title = "alt">a</div>\
-                            <div class="brdr Ctrl abbr">c</div>\
-                            <div class="brdr Shift abbr">s</div>\
-                            <div class="brdr CtrlAlt abbr">ca</div>\
-                            <div class="brdr AltShift abbr">as</div>\
-                            <div class="brdr CtrlShift abbr">cs</div>\
-                            <div class="brdr CtrlAltShift abbr">cas</div>\
-                            <div class="brdr Simple "></div>\
-                            <div class="brdr Alt-title droppable"></div>\
-                            <div class="brdr Ctrl-title droppable"></div>\
-                            <div class="brdr Shift-title droppable"></div>\
-                            <div class="brdr CtrlAlt-title droppable"></div>\
-                            <div class="brdr AltShift-title droppable"></div>\
-                            <div class="brdr CtrlShift-title droppable"></div>\
-                            <div class="brdr CtrlAltShift-title droppable"></div>\
-                            <div class="brdr Simple-title droppable"></div>\
-                        </div>';
-    }
-    return subtableHTML;
-}
-
-function generateKeyboardGrid() {
-    var KeyboardGridHTML = '';
-
-    for (rowButtons in arrayKeyboardKeyValues) {
-        for (keyButton of arrayKeyboardKeyValues[rowButtons]) {
-            KeyboardGridHTML += '<div class="char' + keyButton + '">' + getSubgridWithModifiers(keyButton) + '</div>';
-        }
-    };
-    // console.log(KeyboardGridHTML);
-    document.getElementById('keybordGrid').innerHTML = KeyboardGridHTML;
-}
-
-
-// dragula([document.querySelector('#left'), document.querySelector('#right')], {});
-function onDragMaster() {
-    const boxNodes = document.querySelectorAll('.droppable'); // returns NodeList
-
-    const draggableBoxes = Array.prototype.slice.call(boxNodes);
-
-
-    dragula(draggableBoxes, )
-        .on('drag', (el) => {
-            el.style.width = '14px';
-        })
-        .on('drop', (el, targret) => {
-            el.style.width = (targret.offsetWidth - 5) + 'px';
-        })
-}
-
-
-
-
-// function showHideDescriptionCommand(el, targret) {
-//     // alert(el.childNodes[1].offsetWidth)
-//     if (+targret.offsetWidth < 35) {
-//         // alert(el.nodeName)
-//         el.childNodes[3].style.display = 'none';
-//     } else {
-//         el.childNodes[3].style.display = '';
-//     }
-// }
-
-
-
-function showHide() {
-    let collectionCommandDescr = document.getElementsByClassName('command_description');
-    // console.log(collectionCommandDescr)
-
-
-    for (let i = 1; i < collectionCommandDescr.length; i++) {
-        let elem = collectionCommandDescr[i];
-        if (elem.parentNode.getAttribute('class').split(' ')[0] == 'brdr') {
-            elem.style.width = (elem.parentNode.offsetWidth - 5) + 'px';
-        }
     }
 }
