a8cf061d233024417f73dc8ecd7f737b66e53466
mks <katafoxi@gmail.com>
2022-07-01 14:22:57 +0300

    success move

--- keybinds/shortcutEditor/static/shortcutEditor/js/grid.js
+++ keybinds/shortcutEditor/static/shortcutEditor/js/grid.js
@@ -1,58 +1,9 @@
-const keyboardKeys = {
-    rowF1: ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Esc', 'PSc', 'SLk', 'Pse'],
-    row12: ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Bck', 'Ins', 'home', 'PUp'],
-    rowQW: ['', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '/', 'Del', 'End', 'PD'],
-    rowAS: ['', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '„ “', '', 'Enter', '', 'Up', ''],
-    rowZX: ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'mouse-lft', 'mouse-mid', 'mouse-rght', 'Lft', 'Dwn', 'Rght']
-}
-
-const modifiers = ['Alt', 'Ctrl', 'Shift', 'CtrlAlt', 'CtrlShift', 'AltShift', 'CtrlAltShift'];
-
-generateKeyboardGrid();
 onDragMaster();
 window.onresize = changeCommandDescriptionWidth;
 showFlipswiths();
 showOrHideModifiersRows();
 
 
-function generateKeyboardGrid() {
-    var KeyboardGridHTML = '';
-
-    for (rowButtons in keyboardKeys) {
-        for (keyButton of keyboardKeys[rowButtons]) {
-            KeyboardGridHTML += '<div class="char' + keyButton + '">' + getSubgridWithModifiers(keyButton) + '</div>';
-        }
-    };
-    document.getElementById('keybordGrid').innerHTML = KeyboardGridHTML;
-}
-
-function getSubgridWithModifiers(keyboardKey) {
-    let subtableHTML = '';
-    if (keyboardKey == '') {
-        subtableHTML = '';
-    } else {
-        subtableHTML = '<div  class="subtable">\
-                            <div class="brdr Symbol">' + keyboardKey + '</div>\
-                            <div class="brdr Alt abbr" title = "alt">a</div>\
-                            <div class="brdr Ctrl abbr">c</div>\
-                            <div class="brdr Shift abbr">s</div>\
-                            <div class="brdr CtrlAlt abbr">ca</div>\
-                            <div class="brdr AltShift abbr">as</div>\
-                            <div class="brdr CtrlShift abbr">cs</div>\
-                            <div class="brdr CtrlAltShift abbr">cas</div>\
-                            <div class="brdr Simple "></div>\
-                            <div class="brdr Alt-title droppable"></div>\
-                            <div class="brdr Ctrl-title droppable"></div>\
-                            <div class="brdr Shift-title droppable"></div>\
-                            <div class="brdr CtrlAlt-title droppable"></div>\
-                            <div class="brdr AltShift-title droppable"></div>\
-                            <div class="brdr CtrlShift-title droppable"></div>\
-                            <div class="brdr CtrlAltShift-title droppable"></div>\
-                            <div class="brdr Simple-title ' + keyboardKey + ' droppable"></div>\
-                        </div>';
-    }
-    return subtableHTML;
-}
 
 // dragula([document.querySelector('#left'), document.querySelector('#right')], {});
 function onDragMaster() {
@@ -70,9 +21,9 @@ function onDragMaster() {
         })
 }
 
-window.onbeforeunload = function() {
-    return "Есть несохранённые изменения. Всё равно уходим?";
-};
+// window.onbeforeunload = function() {
+//     return "Есть несохранённые изменения. Всё равно уходим?";
+// };
 
 
 function changeCommandDescriptionWidth() {
--- keybinds/shortcutEditor/views.py
+++ keybinds/shortcutEditor/views.py
@@ -2,6 +2,14 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 from .models import *
 
+keyboard_keys = {
+    'rowF1': ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Esc', 'PSc', 'SLk', 'Pse'],
+    'row12': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Bck', 'Ins', 'home', 'PUp'],
+    'rowQW': ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '/', 'Del', 'End', 'PD'],
+    'rowAS': ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '„ “', '', 'Enter', '', 'Up', ''],
+    'rowZX': ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'mouse_left', 'mouse_middle', 'mouse_right', 'Lft', 'Dwn', 'Rght']
+}
+modifiers = ['Alt', 'Ctrl', 'Shift', 'CtrlAlt', 'CtrlShift', 'AltShift', 'CtrlAltShift']
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить программу', 'url_name': 'add_program'},
@@ -17,10 +25,48 @@ def index(request):
         'title': 'Редактор комбинаций',
         'programs': programs,
         # 'program_commands': program_commands,
-        'prog_selected':0
+        'prog_selected':0,
+        'keyboard_keys':keyboard_keys
     }
     res = render(request, 'shortcutEditor/index.html', context=context)
     return res
+
+def generate_keyboard_grid():
+    keyboard_grid_hmtl = ''
+    for row in keyboard_keys:
+        for key in keyboard_keys[row]:
+            keyboard_grid_hmtl += '<div class="char' + key + '">' + get_subgrid_with_modifiers(key) + '</div>'
+    return keyboard_grid_hmtl
+    # document.getElementById('keybordGrid').innerHTML = KeyboardGridHTML
+
+
+def get_subgrid_with_modifiers(key):
+        subtable_html = ''
+        if (key == ''):
+            subtable_html = ''
+        else:
+            subtable_html = '<div  class="subtable">\
+                                <div class="brdr Symbol">' + key + '</div>\
+                                <div class="brdr Alt abbr" title = "alt">a</div>\
+                                <div class="brdr Ctrl abbr">c</div>\
+                                <div class="brdr Shift abbr">s</div>\
+                                <div class="brdr CtrlAlt abbr">ca</div>\
+                                <div class="brdr AltShift abbr">as</div>\
+                                <div class="brdr CtrlShift abbr">cs</div>\
+                                <div class="brdr CtrlAltShift abbr">cas</div>\
+                                <div class="brdr Simple "></div>\
+                                <div class="brdr Alt-title droppable"></div>\
+                                <div class="brdr Ctrl-title droppable"></div>\
+                                <div class="brdr Shift-title droppable"></div>\
+                                <div class="brdr CtrlAlt-title droppable"></div>\
+                                <div class="brdr AltShift-title droppable"></div>\
+                                <div class="brdr CtrlShift-title droppable"></div>\
+                                <div class="brdr CtrlAltShift-title droppable"></div>\
+                                <div class="brdr Simple-title ' + key + ' droppable"></div>\
+                            </div>'
+
+        return subtable_html
+
 
 
 def about(request):
@@ -52,7 +98,8 @@ def show_program_commands(request, program_id):
         'title': 'Редактор комбинаций',
         'programs': programs,
         'program_commands': program_commands,
-        'prog_selected':program_id
+        'prog_selected':program_id,
+        'keyboard_keys': keyboard_keys
     }
     return render(request, 'shortcutEditor/index.html', context=context)
 
