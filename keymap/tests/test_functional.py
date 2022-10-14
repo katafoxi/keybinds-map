33bf8564c5da360336c6a2ee784b1cada144c74c
Maksim Lapin <katafoxi@gmail.com>
2022-10-14 19:45:25 +0300

    add test_register

--- keymap/tests/test_functional.py
+++ keymap/tests/test_functional.py
@@ -1,45 +1,72 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# import time
-# import unittest
-#
-#
-# class NewVisitorTest(unittest.TestCase):
-#
-#     def setUp(self):
-#         """ установка"""
-#         self.driver = webdriver.Firefox()
-#
-#     def tearDown(self):
-#         """ демонтаж """
-#         self.driver.quit()
-#
-#     def test_can_start_a_list_and_retrieve_it_later(self):
-#         """ тест: можно начать список и получить его позже """
-#         # Заход на домашнюю страницу
-#         self.driver.get('http://localhost:8000')
-#
-#         # Видим нужный title
-#         self.assertIn('Выбор программы для редактора', self.driver.title)
-#         self.driver.find_element(by='id', value='keyboardGrid')
-#
-#     def test_can_login(self):
-#         """ тест: вход на сайт"""
-#
-#         self.driver.get('http://localhost:8000/login')
-#         self.assertIn('Регистрация', self.driver.find_element(by=By.CLASS_NAME, value='last').text)
-#         input_name = self.driver.find_element(by='id', value='id_username')
-#         input_name.send_keys('test')
-#         input_pass = self.driver.find_element(by='id', value='id_password')
-#         input_pass.send_keys('123456789@')
-#         input_pass.send_keys(Keys.ENTER)
-#         WebDriverWait(
-#             driver=self.driver,
-#             timeout=10,
-#         ).until(
-#             EC.url_to_be('http://localhost:8000/')
-#         )
-#         self.assertIn('test', self.driver.find_element(by=By.CLASS_NAME, value='last').text)
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class NewVisitorTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        """ установка"""
+        self.driver = webdriver.Firefox()
+        self.username = 'test'
+        self.email = 'testuser@gmail.com'
+        self.password = '123456789@'
+
+    def tearDown(self):
+        """ демонтаж """
+        self.driver.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """ Тест: можно начать список и получить его позже """
+        # Заход на домашнюю страницу
+        self.driver.get(self.live_server_url)
+
+        # Видим нужный title
+        self.assertIn('Выбор программы для редактора', self.driver.title)
+        self.driver.find_element(by='id', value='keyboardGrid')
+
+    def test_can_register(self):
+        """ тест: возможность регистрации"""
+
+        self.driver.get('%s%s' % (self.live_server_url, '/register/'))
+        self.driver.find_element(by='id', value='id_username').send_keys(self.username)
+        self.driver.find_element(by='id', value='id_email').send_keys(self.email)
+        self.driver.find_element(by='id', value='id_password1').send_keys(self.password)
+        self.driver.find_element(by='id', value='id_password2').send_keys(self.password)
+        self.driver.find_element(By.XPATH, '//button[text()="Регистрация"]').click()
+        self.finisher()
+
+    def test_can_login(self):
+        """ Тест: вход на сайт"""
+
+        user = User.objects.create_user(
+            username=self.username,
+            email=self.email,
+            password=self.password
+        )
+        user.save()
+        self.driver.get('%s%s' % (self.live_server_url, '/login/'))
+        self.assertIn('Регистрация', self.driver.find_element(by=By.CLASS_NAME, value='last').text)
+        self.driver.find_element(by='id', value='id_username').send_keys(self.username)
+        password = self.driver.find_element(by='id', value='id_password')
+        password.send_keys(self.password)
+        password.send_keys(Keys.ENTER)
+        self.driver.find_element(By.XPATH, '//button[text()="Войти"]').click()
+        self.finisher()
+
+    def finisher(self):
+        WebDriverWait(
+            driver=self.driver,
+            timeout=10,
+        ).until(
+            EC.url_to_be(self.live_server_url + '/')
+        )
+        self.assertIn(self.username, self.driver.find_element(by=By.CLASS_NAME, value='last').text)
+        u = User.objects.get(username=self.username)
+        u.delete()
+
+
