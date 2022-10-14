d9f2e1649c6ae91b5a082fdca65c6a420ff372a9
Maksim Lapin <katafoxi@gmail.com>
2022-10-14 20:04:52 +0300

    urls ordering

--- keymap/urls.py
+++ keymap/urls.py
@@ -1,23 +1,15 @@
-from django.http import request
-from django.urls import path, include
-from .views import *
+from django.urls import path
+
+from keymap.views import *
 
 urlpatterns = [
     path("", Index.as_view(), name="main"),
     path("contact/", contact, name="contact"),
+    path("register/", RegisterUser.as_view(), name="register"),
     path("login/", LoginUser.as_view(), name="login"),
     path("logout/", logout_user, name="logout"),
-    path("register/", RegisterUser.as_view(), name="register"),
-    path("addprogram/", AddProgram.as_view(), name="add_program"),
     path("program/<slug:slug>/", ShowProgramCommands.as_view(), name="program"),
-    path(
-        "program/<slug:slug>/<int:id>",
-        ShowProgramCommands.as_view(),
-        name="settings_file",
-    ),
-    path(
-        "program/<slug:slug>/analise",
-        analise_settings_file,
-        name="settings_file_analise",
-    ),
+    path("program/<slug:slug>/<int:id>", ShowProgramCommands.as_view(), name="settings_file", ),
+    path("program/<slug:slug>/analise", analise_settings_file, name="settings_file_analise", ),
+    path("addprogram/", AddProgram.as_view(), name="add_program"),
 ]
