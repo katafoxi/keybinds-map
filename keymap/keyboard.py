250497d712ae08e98e62145361c07ff68c375375
Maksim Lapin <katafoxi@gmail.com>
2022-10-16 20:13:34 +0300

    перенесены tab and space

--- keymap/keyboard.py
+++ keymap/keyboard.py
@@ -4,26 +4,19 @@ from keymap.models import Command
 
 class Keyboard:
     buttons_front = {
-        'rowF1': ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', '␛', '⎙', 'SLk', '⏸',
-                  'N÷'],
+        'rowF1': ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Esc', '🎦', 'SLk', 'Pau', 'N÷'],
         'row12': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-_', '+=', '⌫', 'Ins', '🏠', 'P▲', 'N×'],
         'rowQW': ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[{', ']}', '\\|', '⌦', 'End', 'P▼', 'N-'],
-        'rowAS': ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';:', '„ “', '⭾', '⏎', '⏘', '🡅', '', 'N+'],
-        'rowZX': ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',<', '.>', '/?', '🖰 L', '🖰M', '🖰R', '🡄', '🡇', '🡆', 'N⏎']
+        'rowAS': ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';:', '„ “', '', '⏎', '↹', '↑', '🚀', 'N+'],
+        'rowZX': ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',<', '.>', '/?', '.L', '.M', '.R', '←', '↓', '→', 'N⏎']
     }
 
     buttons_back = {
-        'rowF1': ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'escape',
-                  'print screen', 'scroll lock', 'pause', 'divide'],
-        'row12': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'minus', 'equals', 'back_space', 'insert',
-                  'home', 'page up', 'multiply'],
-        'rowQW': ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'open_bracket', 'close_bracket', 'back_quote',
-                  'delete', 'end', 'page down',
-                  'subtract'],
-        'rowAS': ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon', 'apostrophe', 'tab', 'enter', 'space',
-                  'up', 'None3', 'add'],
-        'rowZX': ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'button1', 'button2', 'button3',
-                  'left', 'down', 'right', '']
+        'rowF1': ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'escape', 'print screen', 'scroll lock', 'pause', 'divide'],
+        'row12': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'minus', 'equals', 'back_space', 'insert', 'home', 'page up', 'multiply'],
+        'rowQW': ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'open_bracket', 'close_bracket', 'back_quote', 'delete', 'end', 'page down', 'subtract'],
+        'rowAS': ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon', 'apostrophe', 'None3', 'enter', 'tab', 'up', 'space', 'add'],
+        'rowZX': ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'button1', 'button2', 'button3', 'left', 'down', 'right', '']
     }
 
     @staticmethod
@@ -74,7 +67,7 @@ class Keyboard:
                     keyboard_buttons[button].update({modifiers: command_repr})
                 else:
                     print(button)
-                    if button=='b':
+                    if button == 'b':
                         print('Странная хуйня')
                         print(Keyboard.buttons_front)
         return keyboard_buttons
